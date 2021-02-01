48a820cba7add636dac272b2730511f5d444b167
MinhVQ <mnhba220@gmail.com>
2021-02-01 10:37:41 +0700

    add feature/add-to-favorite

--- src/main/java/com/example/demo/services/business/DiaryService.java
+++ src/main/java/com/example/demo/services/business/DiaryService.java
@@ -1,4 +1,10 @@
 package com.example.demo.services.business;
 
+import com.example.demo.exceptions.ResponseDetail;
+import com.example.demo.models.outs.DiaryDto;
+import org.springframework.http.ResponseEntity;
+
 public interface DiaryService {
+    ResponseEntity<ResponseDetail<DiaryDto>> favorite(Integer id, boolean isFavorite);
+
 }
